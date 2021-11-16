37b3ed29dfbbbf0ab98301204c9d24e94b68031e
Vasbe <vasbeme@gmail.com>
2021-11-16 13:47:00 +0400

    Добавьте файлы проекта.

---
+++ 9_I_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace _9_I_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите заданную букву: ");
+            char a = char.Parse(Console.ReadLine());
+            int k = 0;
+
+            foreach (String s in File.ReadAllLines(@"C:\Users\vasbe\Desktop\test.txt"))
+            {
+                if (s != null && s.Length > 0 && s[0] == a)
+                    k++;
+            }
+
+            Console.WriteLine("Кол-во строк= " + k);
+            Console.ReadKey();
+        }
+    }
+}
